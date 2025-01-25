04e95f2877ab309d19ed6e6bf53aea6f6236ce55
Dodoslav Novak <dodoslavn@gmail.com>
2025-01-25 03:10:53 +0100

    first

--- terraio/server.js
+++ terraio/server.js
@@ -1,3 +1,15 @@
-'use strict';
+// Import the http module
+const http = require('http');
 
-console.log('Hello world');
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+    // Set the response header
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    // Send a response message
+    res.end('Hello, World!\n');
+});
+
+// Set the server to listen on port 3000
+server.listen(3000, 'localhost', () => {
+    console.log('Server is running at http://localhost:3000/');
